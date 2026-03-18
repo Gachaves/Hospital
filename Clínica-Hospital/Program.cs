6e81a8f89e61caf9e6d4f0a024094633192047f1
Gabriel Chaves Pinheiro <gabriel.pinheiro@sga.pucminas.br>
2026-03-18 14:05:02 -0300

    Adicionar arquivos de projeto.

---
+++ Clínica-Hospital/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Clínica_Hospital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
