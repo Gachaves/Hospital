7b58ab2a47c3625e15c8eb6ba26782019369db0d
Gabriel Chaves Pinheiro <gabriel.pinheiro@sga.pucminas.br>
2026-03-18 14:16:59 -0300

    Adicionar classe Paciente com métodos de consulta
    Implementada a classe `Paciente` no arquivo `Paciente.cs`, incluindo um atributo privado `contato` e uma lista de consultas. Adicionados métodos para agendar consultas, listar consultas e atualizar o contato, conforme o UML.

---
+++ Clínica-Hospital/Paciente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clínica_Hospital
+{
+    public class Paciente
+    {
+        private string contato;
+        private List<Consulta> consultas = new List<Consulta>();
+
+        public Paciente(string contato)
+        {
+            this.contato = contato;
+        }
+
+        // Agendar consulta conforme UML
+        public void AgendarConsulta(Medico m, string data)
+        {
+            Consulta c = new Consulta();
+            c.Marcar(this, m, data);
+            consultas.Add(c);
+        }
+
+        // Listar consultas
+        public List<Consulta> ListarConsultas()
+        {
+            return consultas;
+        }
+
+        // Atualizar contato
+        public void AtualizarContato(string contato)
+        {
+            this.contato = contato;
+        }
+    }
+}
