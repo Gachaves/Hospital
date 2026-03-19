d0444648b7f0930c5f35005f545660b33a5b32b2
josy-vasconcelos <125702327+josy-vasconcelos@users.noreply.github.com>
2026-03-19 10:59:35 -0300

    Enviando classes Consulta e Medico - Grupo 5

---
+++ medico.cs
@@ -0,0 +1,23 @@
+﻿public class Medico
+{
+    public string Nome { get; set; }
+
+    public void AgendarAtendimento(Paciente paciente, string data)
+    {
+        Console.WriteLine($"Médico {Nome} confirmou agenda para {paciente.Nome} em {data}.");
+    }
+    public List<Consulta> ListarConsultas() => new List<Consulta>();
+
+    public void EmitirReceita(Consulta consulta, Receita receita)
+    {
+        
+        if (consulta.Concluida)
+        {
+            Console.WriteLine($"Receita emitida pelo Dr. {Nome} para o paciente {consulta.Paciente.Nome}.");
+        }
+        else
+        {
+            Console.WriteLine("ERRO: Não é possível emitir receita para uma consulta não concluída.");
+        }
+    }
+}
